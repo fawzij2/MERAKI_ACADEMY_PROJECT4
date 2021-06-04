f897eaa72db0fba059d6e930099a5dfa2b7ccdbd
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-04 21:01:31 +0300

    added profile routers and controllers

---
+++ backend/routers/controllers/profile.js
@@ -0,0 +1,35 @@
+const { User } = require("./../../db/models/user");
+const { Case } = require("./../../db/models/case");
+
+// get user by id function
+const getUserById = (req, res) => {
+  const userId = req.params.id;
+  User.findOne({ _id: userId })
+    .then((result) => {
+      res.status=200;
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status=400;
+      res.json(err);
+    });
+};
+
+// get cases by user function
+const getCasesByUser = (req, res) => {
+  const userId = req.params.id;
+  Case.find({ userId: userId })
+    .then((result) => {
+      res.status(200);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status=400;
+      res.json(err);
+    });
+};
+
+module.exports = {
+  getUserById,
+  getCasesByUser,
+};
