4fb30b46803659391cd147ca4ad6066cfb695b62
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-04 21:05:04 +0300

    deleted profile

--- backend/routers/routes/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUserById, getCasesByUser } = require("./../controllers/profile");
-
-const profileRouter = express.Router();
-
-profileRouter.get("/:id/", getUserById);
-profileRouter.get("/:id/cases", getCasesByUser);
-
-module.exports = profileRouter;
