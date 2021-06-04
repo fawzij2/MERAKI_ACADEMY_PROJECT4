e49f8bfd081f8c099af5dea3de6a5a060ddf2a69
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-06-04 20:42:17 +0300

    register done

--- backend/routers/routes/auth/register.js
+++ backend/routers/routes/auth/register.js
@@ -1,1 +1,9 @@
+const express = require("express");
 
+const { register } = require("./../../controllers/register");
+
+const createUser = express.Router();
+
+createUser.post("/",register);
+
+module.exports = createUser;
