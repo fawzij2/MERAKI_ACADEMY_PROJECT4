59eee7441b7dd2df08b27d50a754a3e5c150dd5d
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-04 20:55:40 +0300

    Finished all routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./db/db');
-
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/db");
+const Role = require("./db/models/role");
 const app = express();
 
 //routers
 const home = require("./routers/routes/home");
 const login = require("./routers/routes/auth/login");
-
+const register = require("./routers/routes/auth/register");
+const cases = require("./routers/routes/cases");
+const role = require("./routers/routes/role");
+const profile = require("./routers/routes/profile");
+const authentication = require("./routers/middlewares/authentication");
+const donation = require("./routers/routes/donations");
 
 //built-in middlewares
 app.use(express.json());
@@ -21,8 +26,22 @@ app.use(home);
 //login router
 app.use("/login", login);
 
+//register router
+app.use("/register", register);
+
+//user router
+app.use("/user", profile);
+
+//cases router
+app.use("/cases", cases);
+
+//role router
+app.use("/role", role);
+
+//donation router
+app.use("/donations", donation);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`Server On ${PORT}`);
+  console.log(`Server On ${PORT}`);
 });
