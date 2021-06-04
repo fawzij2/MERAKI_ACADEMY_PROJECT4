e03a4d7728d18acdf3f436acb77c6e939d1f165e
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-04 20:35:07 +0300

    added cases schema

---
+++ backend/db/models/case.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// cases Schema
+const caseSchema = new mongoose.Schema({
+  caseName: { type: String, required: true },
+  category: { type: String, required: true },
+  neededAmount: { type: Number, required: true },
+  address: { type: String },
+  isPrivate: { type: Boolean },
+  isClosed: { type: Boolean },
+  donatedAmount: { type: Number },
+  priority: { type: Number },
+  userId: { type: mongoose.Schema.ObjectId, ref: "User" },
+});
+
+module.exports = mongoose.model("Case", caseSchema);
