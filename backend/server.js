80a4f2ce80f8f27183a3dee2ec5bb39a2f9e63b1
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-04 20:40:55 +0300

    Finished home routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const db = require('./db/db');
 const app = express();
 
 //routers
+const home = require("./routers/routes/home");
+
 
 //built-in middlewares
 app.use(express.json());
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 //app routers
+app.use(home);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
