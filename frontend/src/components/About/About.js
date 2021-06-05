c59fb532235bc4f5c6e1e98039a5a0dfdbaabd1d
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-05 15:19:53 +0300

    Created the about us component without css

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,5 +1,7 @@
 import react from "react";
 import poor from "./poor.jpg"
+import man from "./help.png"
+import house from "./house.png"
 
 const About = () => {
   return (
@@ -7,6 +9,16 @@ const About = () => {
       <div className="about">
         <div className="aboutImg">
           <img src={poor}></img>
+        </div>
+        <div>
+          <h3>ABOUT US</h3>
+          <h1>BLISS is <span>Nonprofit</span> Organization <span>For Helping</span> needed cases. </h1>
+          <p>Bliss goal is giving help to those in need of it. As a humanitarian act, which involves giving money, goods or time and effort to those who need it. It is done without expecting something in return.</p>
+          <div>
+          <img src={house}></img> <span>Save Buildings.</span>
+          <img src={man}></img> <span>Help poor.</span>
+
+          </div>
         </div>
       </div>
     </div>
