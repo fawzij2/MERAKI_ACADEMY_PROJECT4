969efab00a0cf00310a37fde72ba71af7d853913
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-06-06 16:26:18 +0300

    add input

--- frontend/src/components/AddNewCase/index.js
+++ frontend/src/components/AddNewCase/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
 export default function AddNewCase() {
+const [caseName, setCaseName] = useState("")
+const [category, setCategory] = useState("")
+const [neededAmount, setNeededAmount] = useState(0)
+const [address, setAddress] = useState("")
+const [isPrivate, setIsPrivate] = useState("Yes")
+
+
+
     return (
         <div>
+            <input type="text" placeholder = "case name here"  onChange={(e)=>{
+                setCaseName(e.target.value)
+            }}  />
+
+            <input type="text" placeholder=""/>
+            <input type="number" placeholder="needed amount"  onChange={(e)=>{
+              setNeededAmount(e.target.value)  
+            }}  />
+            <input type="text" placeholder="address here" onChange={(e)=>{
+                setAddress(e.target.value)
+
+            }} />
+            <input type="" placeholder=""/>
+
+            
+
             
         </div>
     )
