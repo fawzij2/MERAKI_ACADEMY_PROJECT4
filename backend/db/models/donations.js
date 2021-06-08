efcf77157f82b701fa2819271cc54bd62f787358
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-06-08 21:49:40 +0300

    update donation

--- backend/db/models/donations.js
+++ backend/db/models/donations.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const donationSchema = new mongoose.Schema({
   caseId: { type: mongoose.Schema.ObjectId, ref: "Case" },
   donorId: { type: mongoose.Schema.ObjectId, ref: "User" },
+  donationAmount :{type: String}
 });
 
 module.exports = mongoose.model("Donation", donationSchema);
