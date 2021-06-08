ec371e0324d10e82c5d143a5869e6927a4a99a04
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-08 21:37:14 +0300

    more modificati9ons to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,10 @@ import { Route } from "react-router-dom";
 import Navigation from "./components/navigation/index";
 import  Home  from "./components/home/index";
 import ClosedCases from "./components/closedCases/ClosedCases";
-//import AvaliableCases from "./components/AvaliableCase";
-import About from './components/About/About';
 import Login from './components/auth/login/index';
 import Register from './components/auth/register/index';
 import AddNewCase from "./components/AddNewCase/index"
+import CaseSearch from "./components/cases_search";
 
 
 const App = () => {
@@ -21,7 +20,7 @@ const App = () => {
         <Route exact path="/login" render={()=><Login path={path} setToken={setToken} />} />
         <Route exact path="/register" component={Register}/>
 		    <Route exact path="/cases/closed" render={()=><ClosedCases setPath={setPath} />}/>
-		    <Route path="/cases/categeories/:categeory" render={()=><AvaliableCases setPath={setPath} />}/>
+		    <Route path="/cases/categeories/:categeory" render={()=>CaseSearch setPath={setPath} />}/>
         <Route exact path = "/cases/:id" />
         <Route exact path="/cases/create" component={AddNewCase} />
       </div>
