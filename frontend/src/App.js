b22d5b893556a58e4e73eb2c3af2d9afa9b05a1e
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-08 19:51:50 +0300

    added Login/Register Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import React,{useState} from "react";
 import { Route } from "react-router-dom";
 import Navigation from "./components/navigation/index";
 import  Home  from "./components/home/index";
-import ClosedCases from "./components/ClosedCases"
+import ClosedCases from "./components/closedCases/ClosedCases"
 //import AvaliableCases from "./components/AvaliableCase"
 import About from './components/About/About'
+import Login from './components/auth/login/index'
+import Register from './components/auth/register/index'
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
       <div className="App">
         <Navigation />
         <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
 
 		<Route exact path="/cases/closed" render={()=><ClosedCases setPath={setPath}/>}/>
 
