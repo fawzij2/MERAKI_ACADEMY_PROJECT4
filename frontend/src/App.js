adf87482be0b3b7c6d7d117942c3db7af7a82c64
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-06-08 19:32:37 +0300

    update UILogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import { Route } from "react-router-dom";
 import Navigation from "./components/navigation/index";
 import  Home  from "./components/home/index";
 import ClosedCases from "./components/ClosedCases"
-import AvaliableCases from "./components/AvaliableCase"
+//import AvaliableCases from "./components/AvaliableCase"
 import About from './components/About/About'
+
 
 const App = () => {
   const [path, setPath] = useState("/");
@@ -14,8 +15,9 @@ const App = () => {
       <div className="App">
         <Navigation />
         <Route exact path="/" component={Home} />
-		<Route exact path="/cases/closed" render={()=><ClosedCases setPath={setPath}/>}/>
-		<Route exact path="/cases/categeories/" render={()=><AvaliableCases setPath1={setPath1}/>}/>
+       
+		{/* <Route exact path="/cases/closed" render={()=><ClosedCases setPath={setPath}/>}/> */}
+		{/* <Route exact path="/cases/categeories/" render={()=><AvaliableCases setPath1={setPath1}/>}/> */}
       </div>
 
       <About/>
