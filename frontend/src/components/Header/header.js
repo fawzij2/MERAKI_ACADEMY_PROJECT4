0d46966adc305a4956117d9fa76dce7d4320d9ca
BayanAlsafadi <bay.alsafadi@gmail.com>
2021-06-11 17:09:26 +0300

    added header file

---
+++ frontend/src/components/Header/header.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './header.css'
+
+const Header = ()=>{
+    return(
+        <div className="Logo">
+            <h1>BLISS</h1>
+        </div>
+    )
+}
+
+export default Header;
